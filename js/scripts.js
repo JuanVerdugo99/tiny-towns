092404c89167e6ce427fe7c3e8209d52383ce314
Juan L Briseño <lopezjuankar@gmail.com>
2020-06-14 02:04:16 -0500

    Se ha reducido el codigo para que se más dinamico

---
+++ js/scripts.js
@@ -0,0 +1,155 @@
+var tableGame = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+document.addEventListener('DOMContentLoaded', () => {
+	changeResources();
+	changeImage();
+})
+
+function changeImage() {
+	var randomBlue = Math.floor(Math.random() * blues.length);
+	document.getElementById("bluesDisplay").src = blues[randomBlue].name;
+	document.getElementById("bluesDisplay").classList.add(randomBlue);
+
+	var randomOranges = Math.floor(Math.random() * oranges.length);
+	document.getElementById("orangesDisplay").src = oranges[randomOranges].name;
+	document.getElementById("orangesDisplay").classList.add(randomOranges);
+
+	var randomReds = Math.floor(Math.random() * reds.length);
+	document.getElementById("redsDisplay").src = reds[randomReds].name;
+	document.getElementById("redsDisplay").classList.add(randomReds);
+
+	var randomGreens = Math.floor(Math.random() * greens.length);
+	document.getElementById("greensDisplay").src = greens[randomGreens].name;
+	document.getElementById("greensDisplay").classList.add(randomGreens);
+
+	var randomGrays = Math.floor(Math.random() * grays.length);
+	document.getElementById("graysDisplay").src = grays[randomGrays].name;
+	document.getElementById("graysDisplay").classList.add(randomGrays);
+
+	var randomYellows = Math.floor(Math.random() * yellows.length);
+	document.getElementById("yellowsDisplay").src = yellows[randomYellows].name;
+	document.getElementById("yellowsDisplay").classList.add(randomYellows);
+
+	var randomBlacks = Math.floor(Math.random() * blacks.length);
+	document.getElementById("blacksDisplay").src = blacks[randomBlacks].name;
+	document.getElementById("blacksDisplay").classList.add(randomBlacks);
+
+	var randomPinks = Math.floor(Math.random() * pinks.length);
+	document.getElementById("pinksDisplay").src = pinks[randomPinks].name;
+	document.getElementById("pinksDisplay").classList.add(randomPinks);
+}
+
+function changeResources() {
+	for (let index = 1; index <= 3; index++) {
+			let randomResource = Math.floor(Math.random() * (resources.length));
+		switch (randomResource) {
+			case 0:
+				document.getElementById('resourceDisplay'+index).innerHTML = "<button id='wood' class='button' style='background-color: #7A3734; border: 1px solid black; margin: 10px; padding: 5px; color: #fff;'>  Madera </button>";
+				break;
+			case 1:
+				document.getElementById('resourceDisplay'+index).innerHTML = "<button id='wheat' class='button' style='background-color: #FCAE1F; color: black; border: 1px solid black;margin: 10px;padding: 5px;'>  Trigo </button>";
+				break;
+			case 2:
+				document.getElementById('resourceDisplay'+index).innerHTML = "<button id='brick'class='button' style='background-color: #F73324; border: 1px solid black;margin: 10px;padding: 5px;color: #fff;'>  Ladrillo </button>";
+				break;
+			case 3:
+				document.getElementById('resourceDisplay'+index).innerHTML = "<button id='glass' class='button' style='background-color: #00949E; border: 1px solid black;margin: 10px;padding: 5px;color: #fff;'>  Vidrio </button>";
+				break;
+			case 4:
+				document.getElementById('resourceDisplay'+index).innerHTML = "<button id='stone' class='button' style='background-color: #A59E87; border: 1px solid black;margin: 10px;padding: 5px;'>  Piedra </button>";
+				break;
+		}
+	}
+}
+
+
+function watchImage(id, clase) {
+	let src = document.getElementById(id).getAttribute('src');
+	document.getElementById('image-modal').src = src;
+	let position = document.getElementById(id).className;
+	let type;
+	switch (clase) {
+		case 'blues': type = blues; break; case 'reds': type = reds; break; case 'grays': type = grays; break; case 'pinks': type = pinks; break;
+		case 'oranges': type = oranges; break; case 'greens': type = greens; break; case 'yellows': type = yellows; break; case 'blacks': type = blacks; break;
+	}
+	document.getElementById('description').innerHTML = type[position].description;
+}
+
+function check() {
+	if (window.location.hash == '#miModal') {
+		window.location = './index.html';
+	}
+}
+
+var resource = null;
+function chooseResource(id) {
+	if (resource == null) {
+		let button;
+		let x = document.getElementById(id).innerHTML;
+		x = x.split(' ');
+		x = x[1].split('"');
+		resource = x[1];
+		console.log(resource);
+		randomResource = Math.floor(Math.random() * (resources.length));
+		switch (randomResource) {
+			case 0:
+				button = "<button  id='wood' class='button' style='background-color: #4B1610; border: 1px solid black;margin: 10px;padding: 5px;color: #fff;'>  Madera </button>";
+				break;
+			case 1:
+				button = "<button  id='wheat' class='button' style='background-color: #FCAE1F; color: black; border: 1px solid black;margin: 10px;padding: 5px;'>  Trigo </button>";
+				break;
+			case 2:
+				button = "<button  id='brick' class='button' style='background-color: #F73324; border: 1px solid black;margin: 10px;padding: 5px;color: #fff;'>  Ladrillo </button>";
+				break;
+			case 3:
+				button = "<button  id='glass' class='button' style='background-color: #00949E; border: 1px solid black;margin: 10px;padding: 5px;color: #fff;'>  Vidrio </button>";
+				break;
+			case 4:
+				button = "<button  id='stone' class='button' style='background-color: #A59E87; border: 1px solid black;margin: 10px;padding: 5px;'>  Piedra </button>";
+				break;
+		}
+		document.getElementById(id).innerHTML = button
+	} else {
+		alert('Ya tienes un recurso seleccionado.');
+	}
+}
+
+function putResource(idCard) {
+	let card = document.getElementById(idCard);
+	if (resource) {
+		if (!card.classList.contains('disabled')) {
+			card.src = './img/' + resource + '.png'
+			card.classList.add('disabled');
+			tableGame[idCard] = resource.charAt(0).toUpperCase();
+			if (resource == 'wheat') {
+				tableGame[idCard] = 'T';
+			}
+			checkTheTableGame();
+			resource = null;
+		} else {
+			alert('Ya hay un recurso aqui.');
+		}
+	} else {
+		alert('Selecciona un recurso.');
+	}
+}
+
+var almshouseH = /-SSG.-|-.SSG-|-GSS.-|-.GSS-|-G...-S...-S...-|-.G..-.S..-.S..-|-..G.-..S.-..S.-|-...G-...S-...S-|-S...-S...-G...-|-.S..-.S..-.G..-|-..S.-..S.-..G.-|-...S-...S-...G-/;
+
+var almshouse = {
+	patron: 'SSG',
+	horizontal: /-SSG.-|-.SSG-|-GSS.-|-.GSS-/,
+	vertical: /-G...-S...-S...-|-.G..-.S..-.S..-|-..G.-..S.-..S.-|-...G-...S-...S-|-S...-S...-G...-|-.S..-.S..-.G..-|-..S.-..S.-..G.-|-...S-...S-...G-/
+}
+
+function checkTheTableGame() {
+	
+	let newTableGame = '-';
+	for (let x = 0; x < tableGame.length; x++) {
+		newTableGame += tableGame[x];
+		if (x == 3 || x == 7 || x == 11 || x == 15) {
+			newTableGame += '-';
+		}
+	}
+	console.log(almshouseH.test(newTableGame));
+}
